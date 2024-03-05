70591fcc217f99fdc75082ce8d3da48efcfcfd58
Miguel Jaramillo <migueljaramillo879@gmail.com>
2024-03-05 16:39:46 -0500

    refactor remove factory import

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,6 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -16,7 +15,7 @@ use Illuminate\Database\Eloquent\Collection;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * USER ATTRIBUTES
