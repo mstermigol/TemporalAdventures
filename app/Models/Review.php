d4c71d61da1228c289f2df04886662a1f9c0f984
Miguel Jaramillo <migueljaramillo879@gmail.com>
2024-03-05 16:40:43 -0500

    refactor remove factory imports

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,14 +6,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
-    use HasFactory;
-
     /**
      * REVIEW ATTRIBUTES
      * $this->attributes['id'] - int - contains the review primary key (id)
