6ec78f424535d7e239c03db2d8f64aea84d4db73
Miguel Jaramillo <migueljaramillo879@gmail.com>
2024-03-06 07:37:42 -0500

    add header to the admin layour

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+  <link href="{{ asset('/css/admin.css') }}" rel="stylesheet" />
+  <title>@yield('title', 'Temporal Adventures')</title>
+</head>
+
+<body>
+
+</body>
