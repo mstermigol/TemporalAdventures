c3acdc732e666bdb9e391ed0a8bcc43d3eb99f64
fonsek <dfonsecalara27@gmail.com>
2024-03-08 00:49:37 -0500

    add community-post migration

--- database/migrations/2024_03_05_010603_create_communityposts_table.php
+++ database/migrations/2024_03_05_010603_create_communityposts_table.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+    Author: David Fonseca
+*/
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -11,8 +15,23 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('communityposts', function (Blueprint $table) {
+        Schema::create('community_posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->string('image');
+            $table->date('dateOfEvent');
+            $table->string('placeOfEvent');
+            $table->enum('category', [
+                'Art and Culture',
+                'War and Conflict',
+                'Music and Entertainment',
+                'Inventions and Discoveries',
+                'Daily Life and Customs',
+                'Exploration and Adventure'
+            ]);
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -22,6 +41,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('communityposts');
+        Schema::dropIfExists('community_posts');
     }
 };
