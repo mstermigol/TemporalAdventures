c9757b7a74a15de3b96bcc90321c4d42c7800ccc
Miguel Jaramillo <migueljaramillo879@gmail.com>
2024-03-09 18:09:04 -0500

    add basic app layout
    Created a basic layout for the app. There are still missing some routes

---
+++ resources/lang/en/app.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+    Author: Miguel Jaramillo
+*/
+
+return [
+    'navbar_items' => [
+        'travels' => 'Travels',
+        'community_posts' => 'Community Posts',
+    ],
+];
---
+++ resources/lang/es/app.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+    Author: Miguel Jaramillo
+*/
+
+return [
+    'navbar_items' => [
+        'travels' => 'Viajes',
+        'community_posts' => 'publicaciones de la comunidad',
+    ],
+];
