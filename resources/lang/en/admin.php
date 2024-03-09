71eb415be1cf0595102358517b1de2cdcb71aa32
Miguel Jaramillo <migueljaramillo879@gmail.com>
2024-03-08 22:10:14 -0500

    add lang for the admin layout

---
+++ resources/lang/en/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+    Author: Miguel Jaramillo
+*/
+
+return [
+    'side_bar' => [
+        'users' => 'Users',
+        'travels' => 'Travels',
+        'community_posts' => 'Community Posts',
+        'reviews' => 'Reviews',
+        'orders' => 'Orders',
+        'items' => 'Items',
+    ],
+];
---
+++ resources/lang/es/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+    Author: Miguel Jaramillo
+*/
+
+return [
+    'side_bar' => [
+        'users' => 'Usuarios',
+        'travels' => 'Viajes',
+        'community_posts' => 'Publicaciones de la comunidad',
+        'reviews' => 'Reseñas',
+        'orders' => 'Pedidos',
+        'items' => 'Artículos',
+    ],
+];
