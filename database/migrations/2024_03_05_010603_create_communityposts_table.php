b2701cf8c1a48e32f8dc664fb250fb0ab18f1d60
fonsek <dfonsecalara27@gmail.com>
2024-03-09 20:36:41 -0500

    add commmunity post migration

--- database/migrations/2024_03_05_010603_create_communityposts_table.php
+++ database/migrations/2024_03_05_010603_create_communityposts_table.php
@@ -7,6 +7,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Enums\CategoryEnum;
 
 return new class extends Migration
 {
@@ -19,17 +20,10 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->string('description');
-            $table->string('image');
-            $table->date('dateOfEvent');
-            $table->string('placeOfEvent');
-            $table->enum('category', [
-                'Art and Culture',
-                'War and Conflict',
-                'Music and Entertainment',
-                'Inventions and Discoveries',
-                'Daily Life and Customs',
-                'Exploration and Adventure'
-            ]);
+            $table->string('image')->nullable();
+            $table->date('date_of_event');
+            $table->string('place_of_event');
+            $table->enum('category', CategoryEnum::values());
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
             $table->timestamps();
--- database/migrations/2024_03_05_010647_create_travels_table.php
+++ database/migrations/2024_03_05_010647_create_travels_table.php
@@ -1,15 +1,10 @@
 <?php
 
-/*
-    Author: David Fonseca
-*/
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,22 +12,6 @@ return new class extends Migration
     {
         Schema::create('travels', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->string('description');
-            $table->string('place');
-            $table->date('dateOfDestination');
-            $table->integer('price');
-            $table->date('startDate');
-            $table->date('endDate');
-            $table->string('image');
-            $table->enum('category', [
-                'Art and Culture',
-                'War and Conflict',
-                'Music and Entertainment',
-                'Inventions and Discoveries',
-                'Daily Life and Customs',
-                'Exploration and Adventure'
-            ]);
             $table->timestamps();
         });
     }
