8062229125fc7d01f22477410a7d172dae6b3190
Sergio Córdoba <scordobamuriel@gmail.com>
2024-03-20 08:35:10 -0500

    refactor app logo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
   <script src="https://kit.fontawesome.com/be50e46cfb.js" crossorigin="anonymous"></script>
   <link href="{{ asset('/css/admin.css') }}" rel="stylesheet" />
-  <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+  <link rel="shortcut icon" href="{{ url('/images/logo-no-bg.png') }}" type="image/x-icon">
   <title>@yield('title', 'Temporal Adventures')</title>
 </head>
 
