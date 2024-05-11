3db34331b623473115070d2e82dbc38cb98902ba
mstermigol <mstermigol@users.noreply.github.com>
2024-05-11 18:55:54 +0000

    Refactor PHP Linting (Pint)

--- app/Http/Controllers/Admin/AdminReviewController.php
+++ app/Http/Controllers/Admin/AdminReviewController.php
@@ -33,11 +33,6 @@ class AdminReviewController extends Controller
             $currentPage,
             ['path' => route('admin.review.index')]
 
-
-
-
-
-            
         );
 
         $viewData = [];
